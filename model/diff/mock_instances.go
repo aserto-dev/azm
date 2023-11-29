5dc8b377e898aa14a772c001121dd3b5d5836569
Gert Drapers <gert.drapers@live.com>
2023-11-29 15:43:44 -0800

    parser test listner

--- model/diff/mock_instances.go
+++ model/diff/mock_instances.go
@@ -5,34 +5,35 @@
 package diff
 
 import (
-	gomock "github.com/golang/mock/gomock"
 	reflect "reflect"
+
+	gomock "github.com/golang/mock/gomock"
 )
 
-// MockInstances is a mock of Instances interface
+// MockInstances is a mock of Instances interface.
 type MockInstances struct {
 	ctrl     *gomock.Controller
 	recorder *MockInstancesMockRecorder
 }
 
-// MockInstancesMockRecorder is the mock recorder for MockInstances
+// MockInstancesMockRecorder is the mock recorder for MockInstances.
 type MockInstancesMockRecorder struct {
 	mock *MockInstances
 }
 
-// NewMockInstances creates a new mock instance
+// NewMockInstances creates a new mock instance.
 func NewMockInstances(ctrl *gomock.Controller) *MockInstances {
 	mock := &MockInstances{ctrl: ctrl}
 	mock.recorder = &MockInstancesMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockInstances) EXPECT() *MockInstancesMockRecorder {
 	return m.recorder
 }
 
-// ObjectTypes mocks base method
+// ObjectTypes mocks base method.
 func (m *MockInstances) ObjectTypes() ([]string, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ObjectTypes")
@@ -41,13 +42,13 @@ func (m *MockInstances) ObjectTypes() ([]string, error) {
 	return ret0, ret1
 }
 
-// ObjectTypes indicates an expected call of ObjectTypes
+// ObjectTypes indicates an expected call of ObjectTypes.
 func (mr *MockInstancesMockRecorder) ObjectTypes() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ObjectTypes", reflect.TypeOf((*MockInstances)(nil).ObjectTypes))
 }
 
-// RelationTypes mocks base method
+// RelationTypes mocks base method.
 func (m *MockInstances) RelationTypes() ([]*RelationKind, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "RelationTypes")
@@ -56,7 +57,7 @@ func (m *MockInstances) RelationTypes() ([]*RelationKind, error) {
 	return ret0, ret1
 }
 
-// RelationTypes indicates an expected call of RelationTypes
+// RelationTypes indicates an expected call of RelationTypes.
 func (mr *MockInstancesMockRecorder) RelationTypes() *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RelationTypes", reflect.TypeOf((*MockInstances)(nil).RelationTypes))
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -16,11 +16,36 @@ func NewTreeShapeListener() *TreeShapeListener {
 	return new(TreeShapeListener)
 }
 
-func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+func (l *TreeShapeListener) ExitUnionRel(c *parser.UnionRelContext) {
+	fmt.Println("ExitUnionRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitIntersectRel(c *parser.IntersectRelContext) {
+	fmt.Println("ExitIntersectRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitExclusionRel(c *parser.ExclusionRelContext) {
+	fmt.Println("ExitExclusionRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitSingleRel(c *parser.SingleRelContext) {
+	fmt.Println("ExitSingleRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitWildcardRel(c *parser.WildcardRelContext) {
+	fmt.Println("ExitWildcardRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitSubjectRel(c *parser.SubjectRelContext) {
+	fmt.Println("ExitSubjectRel", c.GetText())
+}
+
+func (l *TreeShapeListener) ExitArrowRel(c *parser.ArrowRelContext) {
+	fmt.Println("ExitArrowRel", c.GetText())
 }
 
 func TestParser(t *testing.T) {
+	// input := antlr.NewInputStream("user | user:* | group#member | parent->viewer\n")
 	input, _ := antlr.NewFileStream("./parser_test.txt")
 	lexer := parser.NewAzmLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
