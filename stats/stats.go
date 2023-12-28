e4a565fe3cd6ab102d705d5eda90da9bc119e7d4
Ronen Hilewicz <ronen@aserto.com>
2023-12-28 10:35:54 -0500

    Use pointers in Stats

--- stats/stats.go
+++ stats/stats.go
@@ -8,44 +8,71 @@ type Stats struct {
 	ObjectTypes ObjectTypes `json:"object_types,omitempty"`
 }
 
-type ObjectTypes map[model.ObjectName]struct {
+type ObjectTypes map[model.ObjectName]*ObjectType
+
+type ObjectType struct {
 	ObjCount  int32     `json:"_obj_count,omitempty"`
 	Count     int32     `json:"_count,omitempty"`
 	Relations Relations `json:"relations,omitempty"`
 }
 
-type Relations map[model.RelationName]struct {
+type Relations map[model.RelationName]*Relation
+
+type Relation struct {
 	Count        int32        `json:"_count,omitempty"`
 	SubjectTypes SubjectTypes `json:"subject_types,omitempty"`
 }
 
-type SubjectTypes map[model.ObjectName]struct {
+type SubjectTypes map[model.ObjectName]*SubjectType
+
+type SubjectType struct {
 	Count            int32            `json:"_count,omitempty"`
 	SubjectRelations SubjectRelations `json:"subject_relations,omitempty"`
 }
 
-type SubjectRelations map[model.RelationName]struct {
+type SubjectRelations map[model.RelationName]*SubjectRelation
+
+type SubjectRelation struct {
 	Count int32 `json:"_count,omitempty"`
 }
 
 func (s *Stats) ObjectRefCount(on model.ObjectName) int32 {
-	ot := s.ObjectTypes[on]
-	return ot.Count + ot.ObjCount
+	if ot, ok := s.ObjectTypes[on]; ok {
+		return ot.Count + ot.ObjCount
+	}
+
+	return 0
 }
 
 func (s *Stats) RelationRefCount(on model.ObjectName, rn model.RelationName) int32 {
-	return s.ObjectTypes[on].Relations[rn].Count
+	if ot, ok := s.ObjectTypes[on]; ok {
+		if rt, ok := ot.Relations[rn]; ok {
+			return rt.Count
+		}
+	}
+
+	return 0
 }
 
 func (s *Stats) RelationSubjectCount(on model.ObjectName, rn model.RelationName, sn model.ObjectName, sr model.RelationName) int32 {
-	st := s.ObjectTypes[on].Relations[rn].SubjectTypes[sn]
-	if sr != "" {
-		return st.SubjectRelations[sr].Count
+	if ot, ok := s.ObjectTypes[on]; ok {
+		if rt, ok := ot.Relations[rn]; ok {
+			if st, ok := rt.SubjectTypes[sn]; ok {
+				switch {
+				case sr != "":
+					if srt, ok := st.SubjectRelations[sr]; ok {
+						return srt.Count
+					}
+				default:
+					subjCount := int32(0)
+					for _, subj := range st.SubjectRelations {
+						subjCount += subj.Count
+					}
+					return st.Count - subjCount
+				}
+			}
+		}
 	}
 
-	subjCount := int32(0)
-	for _, subj := range st.SubjectRelations {
-		subjCount += subj.Count
-	}
-	return st.Count - subjCount
+	return 0
 }
