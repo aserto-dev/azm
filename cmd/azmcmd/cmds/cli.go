5f3090fd800616a34a965360d83dac3ba9c7e615
Gert Drapers <1533850+gertd@users.noreply.github.com>
2024-09-23 11:25:00 +0200

    model query

--- cmd/azmcmd/cmds/cli.go
+++ cmd/azmcmd/cmds/cli.go
@@ -2,6 +2,7 @@ package cmds
 
 type CLI struct {
 	Common
+	Query   QueryCmd   `cmd:"" help:"query model metadata"`
 	Migrate MigrateCmd `cmd:"" help:"migrate directory v2 metadata to an annotated v3 manifest"`
 	Version VersionCmd `cmd:"" help:"version information"`
 }
