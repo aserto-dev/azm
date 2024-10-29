c3c05d816e90b171f5ebf83461b81ab917303098
Ronen Hilewicz <ronen@aserto.com>
2024-10-29 08:43:51 -0400

    Remove `azmcmd`

--- cmd/azmcmd/cmds/cli.go
+++
@@ -1,7 +0,0 @@
-package cmds
-
-type CLI struct {
-	Common
-	Migrate MigrateCmd `cmd:"" help:"migrate directory v2 metadata to an annotated v3 manifest"`
-	Version VersionCmd `cmd:"" help:"version information"`
-}
