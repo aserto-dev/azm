fe2362cd3c23051460098498f071300a390ae9cc
Ronen Hilewicz <ronen@aserto.com>
2024-12-23 13:16:22 -0500

    Add test for arrow operator using Pipe

--- graph/exec_test.go
+++ graph/exec_test.go
@@ -26,27 +26,38 @@ import (
 //     relations:
 //       mate: user | team#mate | group#member
 //
+//   folder:
+//     relations:
+//       editor: user | group#member
+//     permissions:
+//       can_edit: editor
+//
 //   doc:
 //     relations:
+//       parent: folder
 //       owner: user
 //       editor: user | group#member
 //     permissions:
 //       can_delete: owner
-//       can_edit: can_delete | editor
+//       can_edit: can_delete | editor | parent->can_edit
 //       can_share: owner & editor
 //       only_editor: editor - owner
 
 var execRels = NewRelationsReader(
-	"doc:doc1#owner@user:user1",
-	"doc:doc1#editor@user:user1",
-	"doc:doc1#editor@user:user2",
-	"doc:doc1#editor@group:group1#member",
-	"group:group1#member@user:user3",
+	"doc:doc1#parent@folder:folder1",
+	"doc:doc1#owner@user:doc1owner",
+	"doc:doc1#editor@user:doc1owner",
+	"doc:doc1#editor@user:doc1editor",
+	"doc:doc1#editor@group:doc1owners#member",
+	"group:doc1owners#member@user:doc1ownersmember",
+	"folder:folder1#editor@user:folder1editor",
+	"folder:folder1#editor@group:folder1editors#member",
+	"group:folder1editors#member@user:folder1editorsmember",
 )
 
 var evalTests = []checkTest{
-	{"doc:doc1#owner@user:user1", true},
-	{"doc:doc1#owner@user:user2", false},
+	{"doc:doc1#owner@user:doc1owner", true},
+	{"doc:doc1#owner@user:doc1editor", false},
 }
 
 type (
@@ -73,34 +84,36 @@ func TestExecSet(t *testing.T) {
 	}
 }
 
-var unionTests = []checkTest{
-	{"doc:doc1#can_edit@user:user3", true},
-	{"doc:doc1#can_edit@user:user1", true},
-	{"doc:doc1#can_edit@user:user2", true},
-	{"doc:doc2#can_edit@user:user1", false},
-}
-
-func TestExecUnion(t *testing.T) {
-	assert := assert.New(t)
-	pool := mempool.NewRelationsPool()
-
-	groupMember := &query.Composite{
+var Functions = map[query.Set]query.Expression{
+	*set("group", "member", "user"): &query.Composite{
 		Operator: query.Union,
 		Operands: []query.Expression{
 			set("group", "member", "user"),
 			&query.Pipe{From: set("group", "member", "group", "member"), To: &query.Call{Signature: set("group", "member", "user")}},
 			&query.Pipe{From: set("group", "member", "team", "mate"), To: &query.Call{Signature: set("team", "mate", "user")}},
 		},
-	}
+	},
 
-	teamMate := &query.Composite{
+	*set("team", "mate", "user"): &query.Composite{
 		Operator: query.Union,
 		Operands: []query.Expression{
 			set("team", "mate", "user"),
 			&query.Pipe{From: set("team", "mate", "team", "mate"), To: &query.Call{Signature: set("team", "mate", "user")}},
 			&query.Pipe{From: set("team", "mate", "group", "user"), To: &query.Call{Signature: set("group", "member", "user")}},
 		},
-	}
+	},
+}
+
+var unionTests = []checkTest{
+	{"doc:doc1#can_edit@user:doc1ownersmember", true},
+	{"doc:doc1#can_edit@user:doc1owner", true},
+	{"doc:doc1#can_edit@user:doc1editor", true},
+	{"doc:doc2#can_edit@user:doc1owner", false},
+}
+
+func TestExecUnion(t *testing.T) {
+	assert := assert.New(t)
+	pool := mempool.NewRelationsPool()
 
 	plan := &query.Plan{
 		Expression: &query.Composite{
@@ -111,10 +124,7 @@ func TestExecUnion(t *testing.T) {
 				&query.Pipe{From: set("doc", "editor", "group", "member"), To: &query.Call{Signature: set("group", "member", "user")}},
 			},
 		},
-		Functions: map[query.Set]query.Expression{
-			*set("group", "member", "user"): groupMember,
-			*set("team", "mate", "user"):    teamMate,
-		},
+		Functions: Functions,
 	}
 
 	for _, test := range unionTests {
@@ -128,8 +138,8 @@ func TestExecUnion(t *testing.T) {
 }
 
 var intersectionTests = []checkTest{
-	{"doc:doc1#can_share@user:user1", true},
-	{"doc:doc1#can_share@user:user2", false},
+	{"doc:doc1#can_share@user:doc1owner", true},
+	{"doc:doc1#can_share@user:doc1editor", false},
 }
 
 func TestExecIntersection(t *testing.T) {
@@ -157,8 +167,8 @@ func TestExecIntersection(t *testing.T) {
 }
 
 var negationTests = []checkTest{
-	{"doc:doc1#only_editor@user:user1", false},
-	{"doc:doc1#only_editor@user:user2", true},
+	{"doc:doc1#only_editor@user:doc1owner", false},
+	{"doc:doc1#only_editor@user:doc1editor", true},
 }
 
 func TestExecNegation(t *testing.T) {
@@ -181,6 +191,48 @@ func TestExecNegation(t *testing.T) {
 			result, err := interpreter.Run(checkReq(test.check, false))
 			assert.NoError(err)
 			assert.Equal(test.expected, !result.IsEmpty())
+		})
+	}
+}
+
+var arrowTests = []checkTest{
+	{"doc:doc1#can_edit@user:folder1editor", true},
+	{"doc:doc1#can_edit@user:folder1editorsmember", true},
+}
+
+func TestExecArrow(t *testing.T) {
+	assert := assert.New(t)
+	pool := mempool.NewRelationsPool()
+
+	plan := &query.Plan{
+		Expression: &query.Composite{
+			Operator: query.Union,
+			Operands: []query.Expression{
+				set("doc", "owner", "user"),
+				set("doc", "editor", "user"),
+				&query.Pipe{
+					From: set("doc", "parent", "folder"),
+					To: &query.Composite{
+						Operator: query.Union,
+						Operands: []query.Expression{
+							set("folder", "editor", "user"),
+							&query.Pipe{
+								From: set("folder", "editor", "group", "member"),
+								To:   &query.Call{Signature: set("group", "member", "user")},
+							},
+						},
+					},
+				},
+			},
+		},
+		Functions: Functions,
+	}
+	for _, test := range arrowTests {
+		t.Run(test.check, func(tt *testing.T) {
+			interpreter := query.NewInterpreter(plan, execRels.GetRelations, pool)
+			result, err := interpreter.Run(checkReq(test.check, false))
+			assert.NoError(err)
+			assert.Equal(test.expected, !result.IsEmpty(), test.check)
 		})
 	}
 }
